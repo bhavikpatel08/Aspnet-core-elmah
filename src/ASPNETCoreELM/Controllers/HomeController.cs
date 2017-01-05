c068e9bb00d70be9425ef9d0d5dd960fdd7b5907
Bhavik Patel <bhavikpatel08@gmail.com>
2017-01-05 09:42:57 +0530

    initial
    Initial commit

---
+++ src/ASPNETCoreELM/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASPNETCoreELM.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            throw new Exception("This is elm exception");
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
